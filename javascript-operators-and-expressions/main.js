fb5bb36243455e8d9c8fbd9ac9d125d35352e567
Gaven Loughridge <gavenlou@gmail.com>
2021-03-27 11:52:03 -0700

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+var width = 2;
+var height = 3;
+var area = width * height;
+
+console.log('Value of Area:', area);
+console.log('Type of Area:', typeof area);
+
+var bill = 4;
+var payment = 5;
+var change = bill * payment;
+
+console.log('Value of Change:', change);
+console.log('Type of Change:', typeof change);
+
+var quizzes = 100;
+var midterm = 90;
+var final = 80;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of Grade:', grade);
+console.log('Type of Grade:', typeof grade);
+
+var firstName = 'Gaven';
+var lastName = 'Loughridge';
+var fullName = firstName + ' ' + lastName;
+
+console.log('Value of fullName:', fullName);
+console.log('Type of fullName:', typeof fullName);
+
+var pH = 9;
+var isAcidic = pH < 7;
+
+console.log('Value of isAcidic:', isAcidic);
+console.log('Type of isAcidic:', typeof isAcidic);
+
+var headCount = 23;
+var isSparta = headCount === 300;
+
+console.log('Value of isSparta:', isSparta);
+console.log('Type of isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('Value of motto:', motto);
+console.log('Type of motto:', typeof motto);
