ca1f2ce4131a677f443a195a3c30db9c88a48965
Gaven Loughridge <gavenlou@gmail.com>
2021-03-27 10:20:12 -0700

    Completed JavaScript Primitives and Variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,22 @@
+var fullName = 'Gaven Loughridge';
+
+var isCool = true;
+
+var totalPets = 1;
+
+console.log('Full Name:', fullName);
+console.log('Is Cool:', isCool);
+console.log('Pets Owned:', totalPets);
+console.log('Typeof fullName:', typeof fullName);
+console.log('Typeof isCool:', typeof isCool);
+console.log('Typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('Unknown:', unknown);
+console.log('Typeof unknown:', typeof fullName);
+
+var never;
+
+console.log('Never:', never);
+console.log('Typeof never:', typeof never);
