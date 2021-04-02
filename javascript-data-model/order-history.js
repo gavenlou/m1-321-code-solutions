6a5b2ea3d71386c41871d71f3de5fe3972a9378a
Gaven Loughridge <gavenlou@gmail.com>
2021-04-01 21:45:06 -0700

    Attempt #2

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,10 +5,12 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '114-3941689-8772232',
     deliveredDate: 'Aug 8, 2020',
-    orderTitle: ['JavaScript for impatient programmers'],
-    orderAuthor: ['Rauschmayer, Dr. Axel'],
     returnWindow: 'Sep 7, 2020',
-    price: ['$31.55']
+    items: [{
+      orderTitle: 'JavaScript for impatient programmers',
+      orderAuthor: 'Rauschmayer, Dr. Axel',
+      price: '$31.55'
+    }]
   },
   {
     orderedDate: 'July 19, 2020',
@@ -16,10 +18,12 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '113-9984268-1280257',
     deliveredDate: 'Jul 20, 2020',
-    orderTitle: ['The Timeless Way of Building'],
-    orderAuthor: ['Alexander, Christopher'],
     returnWindow: 'Aug 19, 2020',
-    price: ['$41.33']
+    items: [{
+      orderTitle: 'The Timeless Way of Building',
+      orderAuthor: 'Alexander, Christopher',
+      price: '$41.33'
+    }]
   },
   {
     orderedDate: 'July 4, 2020',
@@ -27,10 +31,12 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '144-2875557-9059409',
     deliveredDate: 'Jul 7, 2020',
-    orderTitle: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'],
-    orderAuthor: [''],
     returnWindow: 'Aug 5, 2020',
-    price: ['$15.98']
+    items: [{
+      orderTitle: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+      orderAuthor: '',
+      price: '$15.98'
+    }]
   },
   {
     orderedDate: 'July 3, 2020',
@@ -38,9 +44,16 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '113-2883177-2648248',
     deliveredDate: 'Jul 5, 2020',
-    orderTitle: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
-    orderAuthor: ['', 'Faroult, Stephane'],
     returnWindow: 'Aug 4, 2020',
-    price: ['$94.95', '$33.99']
+    items: [{
+      orderTitle: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      orderAuthor: '',
+      price: '$94.95'
+    },
+    {
+      orderTitle: 'The Art of Sql',
+      orderAuthor: 'Faroult, Stephane',
+      price: '$33.99'
+    }]
   }
 ];
