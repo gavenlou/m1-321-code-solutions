9a6d929f977bba030f92c5f8f2381a3d29627742
Gaven Loughridge <gavenlou@gmail.com>
2021-04-17 14:33:25 -0700

    Slight streamlining

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -2,9 +2,7 @@
 
 function compact(array) {
   for (var i = 0, result = []; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== 0 && array[i] !== null &&
-      array[i] !== undefined && array[i] !== '' &&
-      !Number.isNaN(array[i])) result.push(array[i]);
+    if (array[i]) result.push(array[i]);
   }
   return result;
 }
