619a1cf4d1e5c4096d49ad40b99e4dbdf6eb3c08
Gaven Loughridge <gavenlou@gmail.com>
2021-04-20 20:24:01 -0700

    Completed javascript-modal

---
+++ javascript-modal/main.js
@@ -0,0 +1,13 @@
+var modal = document.querySelector('.hidden');
+
+// eslint-disable-next-line no-unused-vars
+function toggle(state) {
+  switch (state) {
+    case 'on':
+      modal.className = 'hidden show';
+      break;
+    case 'off':
+      modal.className = 'hidden none';
+      break;
+  }
+}
